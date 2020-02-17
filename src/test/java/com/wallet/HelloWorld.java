bbf25f2064830b44daaf36d702b69163173e8f0f
Deivid Mardegan <deivid.mardegan@gmail.com>
2020-02-17 05:50:28 -0300

    configuração inicial para os testes - junit x spring

---
+++ src/test/java/com/wallet/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.wallet;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class HelloWorld {
+
+	@Test
+	public void testHelloWorld() {
+		assertEquals(1, 1);
+	}
+}
