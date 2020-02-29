117f744d937a622e7a49c15d354aa915f6466cdc
Deivid Mardegan <deivid.mardegan@gmail.com>
2020-02-29 07:16:58 -0300

    adding swagger on project - configuration class and pom

---
+++ src/main/java/com/wallet/config/SwaggerConfig.java
@@ -0,0 +1,13 @@
+package com.wallet.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@Profile("dev")
+public class SwaggerConfig {
+
+}
